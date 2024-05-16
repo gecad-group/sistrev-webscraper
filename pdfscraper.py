62bab38437843c0833780b31ad5a90f96ff7a623
Rui FPB <1211106@isep.ipp.pt>
2024-05-16 14:45:26 +0100

    Start development of the PDFScraper class

--- pdfscraper.py
+++ pdfscraper.py
@@ -1,11 +1,32 @@
 import pycurl
 from io import BytesIO
 import certifi
+from urllib.parse import urlparse
 
 
 class PDFScraper:
+    MOCK_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0'
+
     def __init__(self):
         pass
+
+    def get_publisher_url(self, doi_id: str):
+        buffer = BytesIO()
+
+        url = f'https://doi.org/{doi_id}'
+        c = pycurl.Curl()
+        c.setopt(pycurl.URL, url)
+        c.setopt(pycurl.FOLLOWLOCATION, True)
+        c.setopt(pycurl.CAINFO, certifi.where())
+        c.setopt(pycurl.VERBOSE, False)
+
+        try:
+            c.perform()
+
+            dest_url: str = c.getinfo(pycurl.EFFECTIVE_URL)
+            return urlparse(dest_url).hostname
+        except:
+            return None
 
 
 if __name__ == '__main__':
@@ -29,11 +50,10 @@ if __name__ == '__main__':
     print(id)
 
     import prerunchecks
+
     prerunchecks.check_network()
 
     with open('out.pdf', 'wb') as f:
         c.setopt(pycurl.URL, f"https://ieeexplore.ieee.org/stampPDF/getPDF.jsp?tp=&arnumber={id}")
         c.setopt(pycurl.WRITEDATA, f)
         c.perform()
-
-
