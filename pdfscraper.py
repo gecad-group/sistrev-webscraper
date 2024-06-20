0d475ffd455ac409c6bd2528bea9a6f4ff328e8f
Rui FPB <1211106@isep.ipp.pt>
2024-06-20 14:03:39 +0100

    The PDFScraper now uses scidownl to get PDFs from sci-hub

--- pdfscraper.py
+++ pdfscraper.py
@@ -2,9 +2,61 @@ import pycurl
 from io import BytesIO
 import certifi
 from urllib.parse import urlparse
+from scidownl import scihub_download
+from scidownl.core.task import ScihubTask
+import rispy
+import os
+import re
+from datacleaner import DataCleaner
 
+scihub_urls = ['https://sci-hub.se', 'https://sci-hub.yt']
 
 class PDFScraper:
+    def download_with_doi(doi: str, filename: str):
+        filename = PDFScraper._clean_title(filename)
+        #print('Getting', filename)
+        #scihub_download(doi, paper_type='doi', out=filename)
+
+        for url in scihub_urls:
+            try:
+                ScihubTask(doi, 'doi', out=filename)._run(url)
+
+                filesize = os.stat(filename + ".pdf").st_size
+                if filesize < 300:
+                    os.remove(filename + ".pdf")
+                    raise Exception(f"File is too small ({filesize})")
+
+                return None
+            except Exception as e:
+                print("Not found on", url, ":", e.args[0] if len(e.args) > 0 else "Error didn't have a message")
+                continue
+        print("Failed to get", doi)
+        return doi
+
+    def download_from_ris(filepath: str):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            data = rispy.load(f, encoding='utf-8')
+
+        os.chdir(os.path.dirname(filepath))
+
+        failed: list[str] = []
+
+        for doc in data:
+            print()
+            print('Getting', doc['title'])
+            res = PDFScraper.download_with_doi(doc['doi'], doc['title'])
+            if res != None:
+                failed.append(doc['doi'])
+
+        return failed, len(data)
+
+    def _clean_title(title: str) -> str:
+        tmp = title.replace(':', '').replace('\\', '-').replace('/', '-').replace(' ', '_').replace('"', '')
+        tmp = re.sub('<.*?>', '', tmp)
+
+        return tmp
+
+class PDFScraper_OLD:
     MOCK_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0'
 
     def __init__(self):
@@ -29,31 +81,44 @@ class PDFScraper:
             return None
 
 
+# if __name__ == '__main__':
+#     buffer = BytesIO()
+#
+#     c = pycurl.Curl()
+#     c.setopt(pycurl.URL, 'https://www.doi.org/10.1109/ecai50035.2020.9223186')
+#     c.setopt(pycurl.WRITEDATA, buffer)
+#     c.setopt(pycurl.FOLLOWLOCATION, True)
+#     c.setopt(pycurl.CAINFO, certifi.where())
+#     c.setopt(pycurl.VERBOSE, False)
+#     c.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0')
+#     c.setopt(pycurl.COOKIEJAR, 'cookie')
+#     c.setopt(pycurl.COOKIEFILE, 'cookie')
+#     c.perform()
+#
+#     final_url: str = c.getinfo(pycurl.EFFECTIVE_URL)
+#     id: str = final_url.rstrip('/').rsplit('/')[-1]
+#
+#     print(final_url)
+#     print(id)
+#
+#     import prerunchecks
+#
+#     prerunchecks.check_network()
+#
+#     with open('out.pdf', 'wb') as f:
+#         c.setopt(pycurl.URL, f"https://ieeexplore.ieee.org/stampPDF/getPDF.jsp?tp=&arnumber={id}")
+#         c.setopt(pycurl.WRITEDATA, f)
+#         c.perform()
+
 if __name__ == '__main__':
-    buffer = BytesIO()
+    risfile = input("Path for the .ris file to import: ").replace('"', '')
 
-    c = pycurl.Curl()
-    c.setopt(pycurl.URL, 'https://www.doi.org/10.1109/ecai50035.2020.9223186')
-    c.setopt(pycurl.WRITEDATA, buffer)
-    c.setopt(pycurl.FOLLOWLOCATION, True)
-    c.setopt(pycurl.CAINFO, certifi.where())
-    c.setopt(pycurl.VERBOSE, False)
-    c.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0')
-    c.setopt(pycurl.COOKIEJAR, 'cookie')
-    c.setopt(pycurl.COOKIEFILE, 'cookie')
-    c.perform()
+    failed_downloads, total = PDFScraper.download_from_ris(risfile)
 
-    final_url: str = c.getinfo(pycurl.EFFECTIVE_URL)
-    id: str = final_url.rstrip('/').rsplit('/')[-1]
+    print()
+    print(" Failed downloads ")
+    print("------------------")
+    for doi in failed_downloads:
+        print("https://doi.org/" + doi)
 
-    print(final_url)
-    print(id)
-
-    import prerunchecks
-
-    prerunchecks.check_network()
-
-    with open('out.pdf', 'wb') as f:
-        c.setopt(pycurl.URL, f"https://ieeexplore.ieee.org/stampPDF/getPDF.jsp?tp=&arnumber={id}")
-        c.setopt(pycurl.WRITEDATA, f)
-        c.perform()
+    print("Total Failed", len(failed_downloads), "/", total )
